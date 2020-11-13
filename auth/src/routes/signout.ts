5310065ecea8339c19a37608813adcb8f2166275
joe-avalos <jose.b.avalos@gmail.com>
2020-11-13 13:46:20 -0600

    dev - signout route fix

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -1,10 +1,8 @@
 import express from 'express'
-import {requireAuth} from '@boloyde-gittix/common'
 
 const router = express.Router()
 
 router.post('/api/users/signout',
-  requireAuth,
   (req, res) => {
     req.session = null
     res.send({})
